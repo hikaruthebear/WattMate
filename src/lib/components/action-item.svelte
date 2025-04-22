bb9120ab783361654444a12109dbdc1ae429ce99
HikaruTheBear <beatoalejandro1@gmail.com>
2025-04-22 09:11:27 +0800

    Added todos

--- src/lib/components/action-item.svelte
+++ src/lib/components/action-item.svelte
@@ -1,19 +1,12 @@
 <script>
 	import { Icon } from '$lib';
-	export let icon = '/placeholder.png';
-
-	export let label = '';
-	export let size = '2';
-	export let onclick = () => {
-		console.log('clicked');
-	};
+	let { icon = '/placeholder.png', label = '', size = '2', onclick = () => {} } = $$props;
+	
 </script>
 
-<!-- svelte-ignore a11y_no_static_element_interactions -->
-<!-- svelte-ignore a11y_click_events_have_key_events -->
 <div
-class="text-black-600 hover:bg-primary-200 flex items-center gap-2 rounded-xl transition-all duration-100 ease-in-out"
-	{onclick}
+	class="text-black-600 hover:bg-primary-200 flex items-center gap-2 rounded-xl transition-all duration-100 ease-in-out"
+	{onclick} role="button" tabindex="0" onkeydown={onclick} 
 >
 	<Icon src={icon} {size} />
 	<div>{label}</div>
--- src/routes/admin/dashboard/+page.svelte
+++ src/routes/admin/dashboard/+page.svelte
@@ -1,32 +1,39 @@
 <script>
-	import { AdminSidebar, Header, Card , TableHead, TableRow, Heading} from '$lib';
+	import { AdminSidebar, Header, Card, TableHead, TableRow, Heading , Popover} from '$lib';
 
-	let sidebar = false;
+	let sidebar = true;
 
 	let toggleSidebar = () => {
 		sidebar = !sidebar;
 	};
 </script>
 
+<!-- <Popover {component("fuck")} {}/> -->
+
 <div class="flex h-[100dvh] flex-col overflow-hidden bg-red-500">
-	<header class="h-fit bg-blue-500 fixed w-full z-10">
+	<header class="fixed z-10 h-fit w-full bg-blue-500">
 		<Header onclick={toggleSidebar} />
 	</header>
-	<div class="flex h-full ">
-		<div class=" h-full w-64 bg-green-500 fixed shrink-0 md:relative {sidebar ? 'hidden' : 'flex'} ">
+	<div class="flex h-full">
+		<div
+			class=" fixed h-full w-64 shrink-0 bg-green-500/0 md:relative
+			{sidebar ? 'flex w-full md:w-64' : 'hidden '} outline-black-50/20 z-9 outline-1"
+		>
 			<AdminSidebar />
+			<input type="button"  class="w-full md:hidden" onclick={()=> (sidebar = false)} />
 		</div>
-		<main class="w-full bg-white-200 p-5 flex flex-col gap-5 overflow-y-auto">
-			<div class="flex gap-4 overflow-x-auto mt-10 md:mt-20 shrink-0 p-2">
-				<Card label="Total Amount Paid" amount="10,000.50" icon="/icons/paid.png"/> 
-				<Card label="Current Bill" amount="29,500.50" icon="/icons/currentBill.png"/>
-				<Card label="Monthly Rate" amount="19.50 " icon="/icons/rate.png"/>
-				<Card label ="Total Kilowatts" amount="215 " unit="kWh" icon="/icons/kwh.png"/>
+
+		<main class="bg-white-200 z-1 flex w-full flex-col gap-5 overflow-y-auto p-5">
+			<div class="mt-10 flex shrink-0 gap-4 overflow-x-auto p-2 md:mt-20">
+				<Card label="Total Amount Paid" amount="10,000.50" icon="/icons/paid.png" />
+				<Card label="Current Bill" amount="29,500.50" icon="/icons/currentBill.png" />
+				<Card label="Monthly Rate" amount="19.50 " icon="/icons/rate.png" />
+				<Card label="Total Kilowatts" amount="215 " unit="kWh" icon="/icons/kwh.png" />
 			</div>
 			<div class="bg-white-50 flex flex-col rounded-lg p-8">
 				<Heading heading="Consumers" />
-				<TableHead column1="Name" column2="kWh used" column3="Amount" column4="Status"/>
-				{#each Array(13) as item,i}
+				<TableHead column1="Name" column2="kWh used" column3="Amount" column4="Status" />
+				{#each Array(13) as item, i}
 					<TableRow />
 				{/each}
 			</div>
